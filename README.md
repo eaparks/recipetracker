45526e58968fcfe373772fc12ff84f13b2c087cf
Ed Parks <edward.parks@gmail.com>
2018-07-11 07:42:35 -1000

    Some cleanup of routes, links, file reading.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Recipe Tracker, Saver, Finder, Everythinger
+
+
+PostgreSql:
+if you don't want/need a background service you can just run:
+  pg_ctl -D /usr/local/var/postgres start
+  
+  /usr/local/Cellar/postgresql/10.4
+  
+Setup guide from:
+  https://www.codementor.io/engineerapart/getting-started-with-postgresql-on-mac-osx-are8jcopb
+  
+  
+pg_ctl -D /usr/local/var/postgres start && brew services start postgres
--- app/index.js
+++ app/index.js
@@ -1,5 +1,47 @@
 const fs = require('fs');
 const async = require('async');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'Recipe Vault',
+        chowfun: '/recipes/chowfun',
+        swedishmeatballs: '/recipes/swedishmeatballs',
+    })
+});
+
+app.get('/recipes/chowfun', (request, response) => {
+    fs.readFile('./docs/recipes/chowfun.recipe', function(err, data) {
+        response.render('recipe', { recipetext: data });
+    });
+});
+app.get('/recipes/swedishmeatballs', (request, response) => {
+    fs.readFile('./docs/recipes/swedishmeatballs.recipe', function(err, data) {
+        response.render('recipe', { recipetext: data });
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
 
 
 // async.map(['./docs/recipes/chowfun.recipe', './docs/recipes/swedishmeatballs.recipe'],
@@ -8,10 +50,9 @@ const async = require('async');
 //         console.log(results);
 // });
 
-
 function showRecipe (file) {
     return new Promise((resolve, reject) => {
-        fs.readFile(file, "utf-8", (err, data) => {
+        fs.readFile(file, {encoding: 'utf-8'}, (err, data) => {
             if (err) {
                 return reject (err)
             }
